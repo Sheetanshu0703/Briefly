1e263034a48e57962651533da71a87afe92a66c4
Sheetanshu0703 <sheetanshuyuvi.0703@gmail.com>
2025-04-03 18:23:53 +0530

    Model Trainer

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ sys.path.append(os.path.abspath("src"))
 from Briefly.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from Briefly.logging import logger
 from Briefly.pipeline.stage_02_data_validation import DataValidationTrainingPipeline
+from Briefly.pipeline.stage_03_data_transformation import DataTransformationTrainingPipeline
 
 STAGE_NAME = "Data Ingestion stage"
 try:
@@ -28,3 +29,16 @@ try:
 except Exception as e:
     logger.exception(e)
     raise e
+
+
+
+STAGE_NAME = "Data transformation stage"
+try:
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<<")
+    data_transformation = DataTransformationTrainingPipeline()
+    data_transformation.main()
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<<\n\nx==============================x")
+except Exception as e:
+    logger.exception(e)
+    raise e
+
